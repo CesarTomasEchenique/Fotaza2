1d1a346c90437c2af5ae16be4ff370e519cdbd14
CesarTomasEchenique <119369706+CesarTomasEchenique@users.noreply.github.com>
2026-04-27 21:47:49 -0300

    segundo commit: se anclo el index a una ruta del puerto,se agrego imagenes y css basico

--- scripMain.js
+++ scripMain.js
@@ -2,5 +2,20 @@ const express= require ("express");
 
 const app= express();
 const port =3000;
+const path = require("path");
 
 app.use(express.urlencoded());
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Bienvenido al servidor 3000")
+})
+
+app.get("/fotazahome", (req,res)=>{
+    res.sendFile(path.join(__dirname,"public", "FotazaIndex.html"));
+})
+
+
+app.listen(port,()=>{
+    console.log(`servidor corriendo en : http://localhost:${port}`);
+})
