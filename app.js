52bed9d164cfc911c9030aff365381e419a81f28
CesarTomasEchenique <119369706+CesarTomasEchenique@users.noreply.github.com>
2026-05-13 19:50:07 -0300

    archivo .env y config.js datos de conexion con postgres

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express= require ("express");
+const base = require ("./database");
 
 const app= express();
 const port =3000;
 const path = require("path");
+
+app.get('/usuarios', async (req, res) => {
+
+    try {
+
+        const resultado = await base.query('SELECT NOW()');
+
+        res.json(resultado.rows);
+
+    } catch(error) {
+
+        console.log(error);
+
+        res.status(500).send('Error BD');
+    }
+});
 
 app.use(express.urlencoded());
 app.use(express.static("public"));
